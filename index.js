ed82dd88745fb7aaaccb1f5b95f8b53f3d3420a4
Kenyada McLeod <kenyada.mcleod@ufl.edu>
2022-04-19 19:26:32 -0400

    Created class and new instance

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// YOUR CODE HERE
+// Create a car class
+
+class Car {
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        console.log("BEEP BEEP!");
+    }
+    performMaintenance(){
+        setTimeout(() => {console.log("maintenance complete")}, 3000);
+    }
+};
+
+let mySweetRide = new Car("Pontiac", "Fiero", 1988);
+mySweetRide.honk();
+mySweetRide.performMaintenance();
